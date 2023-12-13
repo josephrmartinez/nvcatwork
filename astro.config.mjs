32cad58131683bd3c79e71d72fcafcb2a0712d10
Joseph Martinez <joseph.r.martinez@gmail.com>
2023-12-13 16:22:10 -0700

    Remove compress

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-import Compress from "astro-compress";
 import partytown from "@astrojs/partytown";
 import sitemap from "@astrojs/sitemap";
 
@@ -9,5 +8,5 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://unwrapped.design',
-  integrations: [tailwind(), Compress(), partytown(), sitemap(), react()]
+  integrations: [tailwind(), partytown(), sitemap(), react()]
 });
